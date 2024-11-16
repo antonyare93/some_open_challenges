93ea64cf0a8eb5c4d57a7e50463c5eeccaab0f72
Antony Arévalo <antonyare_93@hotmail.com>
2024-11-16 11:43:03 -0600

    init_and_first_challenge

---
+++ MoureDev/2024_Challenges/01_helloworld.py
@@ -0,0 +1,27 @@
+"""
+Challenge 1: Hello World
+Exercise:
+- Create a comment with the URL of the language you used.
+- Use the different ways to create comments, one line and multiline.
+- Create a variable (and a constant if the language allows it).
+- Create a variable that represents every primitive data type.
+- Print a 'Hello {language}' message.
+"""
+
+# URL of the language used: https://www.python.org/
+
+""" One line comment
+    Multiline comment
+"""
+
+# Variables With all primitive data types
+language = "Python"          # str (string/cadena)
+integer = 1                  # int (integer)
+float_num = 1.0             # float (float point number)
+boolean = True              # bool (boolean)
+none_type = None           # NoneType
+complex_num = 1 + 2j       # complex (complex number)
+bytes_data = b"Hello"      # bytes
+
+# Print a 'Hello {language}' message.
+print(f"Hello {language}")
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# some_open_challenges
- Here are some challenges that some creators share to sharpen your skills
+# Open challenges
+ Here are some coding challenges that some code creators share, so everyone can practice and improve their skills.
+
+## 1. MoureDev
+ A Spanish developer, who creates coding challenges, that I follow.
+ Go to the MoureDev folder to see the challenges and my own solutions.
+
